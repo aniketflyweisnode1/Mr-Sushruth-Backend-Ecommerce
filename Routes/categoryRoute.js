1a66a2396685cbf8f9b53c9e3d000f03f3938f0e
Deepak Singh <deepakflyweis@gmail.com>
2023-07-31 15:16:07 +0530

    first commit

--- Routes/categoryRoute.js
+++ Routes/categoryRoute.js
@@ -16,8 +16,8 @@ const router = require("express").Router();
 
 router.post("/create", authorizeRoles("admin"), createCategory);
 router.put("/update/:id", authorizeRoles("admin"), updateCategory);
-router.get("/:id", authorizeRoles("admin"), getCategory);
-router.get("/", authorizeRoles("admin"), getallCategory);
+router.get("/:id", /* authorizeRoles("admin"), */ getCategory);
+router.get("/", /* authorizeRoles("admin"), */ getallCategory);
 router.delete("/delete/:id", authorizeRoles("admin"), deleteCategory);
 router.delete("/delete/:id", authorizeRoles("admin"), removeCategory);
 router.post("/subcategory", authorizeRoles("admin"), createSubCategory);
